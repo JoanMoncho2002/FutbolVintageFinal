edb36717e3cc70381483a62bb73cb160c5d8b3f0
Joan Moncho <joanmonvi@gmail.com>
2023-02-06 17:59:27 +0100

    dia 6.2

--- src/main/java/com/joanmoncho/tiendavintage/Controladores/MiControlador.java
+++ src/main/java/com/joanmoncho/tiendavintage/Controladores/MiControlador.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MiControlador {
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
     @GetMapping("/login")
     public String indice(){
-        return "login/login";
+        return "/login/login";
     }
+
+    @GetMapping("/registrar")
+    public String registrar(){
+        return "/login/registrar";
+    }
+
 }
--- src/main/java/com/joanmoncho/tiendavintage/Seguridad/ConfigSeguridad.java
+++ src/main/java/com/joanmoncho/tiendavintage/Seguridad/ConfigSeguridad.java
@@ -1,30 +1,39 @@
 package com.joanmoncho.tiendavintage.Seguridad;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
+@Configuration
 public class ConfigSeguridad {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/register/**").permitAll()
-                                .requestMatchers("/index").permitAll()
-                                .requestMatchers("/users").hasRole("ADMIN")
-                ).formLogin(
-                        form -> form
-                                .loginPage("/login")
-                                .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/users")
-                                .permitAll()
-                ).logout(
-                        logout -> logout
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                .permitAll()
-                );
+        http
+            .authorizeHttpRequests((authorize) ->
+                    authorize.requestMatchers("", "/", "/home", "/registrar","/login", "/css/**","/img/**", "/js/**").permitAll()
+                            .requestMatchers("/index").permitAll()
+                            .requestMatchers("/users").hasRole("ADMIN")
+            ).formLogin((form) -> form
+                            .loginPage("/login")
+                            .permitAll()
+            ).logout(LogoutConfigurer::permitAll);
         return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("root")
+                        .password("1234")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
